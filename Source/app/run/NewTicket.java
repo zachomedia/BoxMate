2764c51d8ce6abe9c9bd943cfed08be58fe93a69
Zachary Seguin <zseguin@me.com>
2012-06-04 18:18:59 -0400

    Finished GUI for Customer Information. TODO: - GUI for Ticket info, buttons.

--- Source/app/run/NewTicket.java
+++ Source/app/run/NewTicket.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 
 /**
  * Shows the GUI required to create a new ticket.
- * 
+ *
  * @author Zachary Seguin
  * @version 1.0.0 (4/6/2012)
  * @since 1.0.0
@@ -16,7 +16,8 @@ public class NewTicket extends JFrame
 {
 	//Declare and initialize constants
 	private final int PADDING_SIZE = 10;
-	
+	private final String [] PROVINCES = {"Ontario", "Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador", "Nortwest Territories", "Nova Scotia", "Nunavut", "Prince Edward Island", "Quebec", "Saskatchewan", "Yukon"};
+
 	//Declare GUI components.
 	private JPanel customerInformationPanel;
 		private JTextField txtFirstName;
@@ -25,7 +26,7 @@ public class NewTicket extends JFrame
 		private JTextField txtAddressStreet;
 		private JTextField txtAddressStreetSufix;
 		private JTextField txtAddressCity;
-		private JTextField txtAddressProvince;
+		private JComboBox cboAddressProvince;
 		private JTextField txtAddressPostalCode;
 		private JTextField txtPhoneAreaCode;
 		private JTextField txtPhonePrefix;
@@ -33,24 +34,26 @@ public class NewTicket extends JFrame
 		private JTextField txtEmailAddress;
 		private JTextField txtEmailAddressDomain;
 		private JTextField txtEmailAddressTLD;
-	
+
 	/**
 	 * Creates the GUI.
-	 * 
+	 *
 	 * @since 1.0.0
 	 */
 	public NewTicket()
 	{
 		//Setup the GUI
 		this.setTitle("New Ticket | " + Application.NAME);
-		this.setSize(600, 400);
+		this.setSize(800, 400);
+		//this.setResizable(false);
+		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		this.initializeGUI();
-		
+
 		this.setVisible(true);
 	}//End of constructor
-	
+
 	/**
 	 * Initialize the GUI components.
 	 *
@@ -59,25 +62,89 @@ public class NewTicket extends JFrame
 	private void initializeGUI()
 	{
 		//Setup the customer information panel
-		this.customerInformationPanel = new JPanel(new FlowLayout());
+		this.customerInformationPanel = new JPanel(new GridLayout(3, 1, 5, 5));
 		this.customerInformationPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createTitledBorder("Customer"), BorderFactory.createEmptyBorder(PADDING_SIZE,PADDING_SIZE,PADDING_SIZE,PADDING_SIZE)));
-		
-		this.txtFirstName = new JTextField();
-		this.txtLastName = new JTextField();
-		this.txtAddressHouseNumber = new JTextField();
-		this.txtAddressStreet = new JTextField();
-		this.txtAddressStreetSufix = new JTextField();
-		this.txtAddressCity = new JTextField();
-		this.txtAddressProvince = new JTextField();
-		this.txtAddressPostalCode = new JTextField();
-		this.txtPhoneAreaCode = new JTextField();
-		this.txtPhonePrefix = new JTextField();
-		this.txtPhoneLine = new JTextField();
-		this.txtEmailAddress = new JTextField();
-		this.txtEmailAddressDomain = new JTextField();
-		this.txtEmailAddressTLD = new JTextField();
-		
+
+		//Intialize all the customer information GUI components
+		this.txtFirstName = new JTextField(15);
+		this.txtLastName = new JTextField(15);
+		this.txtAddressHouseNumber = new JTextField(3);
+		this.txtAddressStreet = new JTextField(15);
+		this.txtAddressStreetSufix = new JTextField(5);
+		this.txtAddressCity = new JTextField(15);
+		this.cboAddressProvince = new JComboBox(PROVINCES);
+		this.txtAddressPostalCode = new JTextField(8);
+		this.txtPhoneAreaCode = new JTextField(3);
+		this.txtPhonePrefix = new JTextField(3);
+		this.txtPhoneLine = new JTextField(3);
+		this.txtEmailAddress = new JTextField(10);
+		this.txtEmailAddressDomain = new JTextField(10);
+		this.txtEmailAddressTLD = new JTextField(6);
+
+		//Setings for the above GUI components
+		this.cboAddressProvince.setEditable(false);
+
+		//Add all the customer information GUI components and their labels to the custome information panel
+		JPanel firstRow = new JPanel(new GridLayout(2, 2, 5, 5));
+		firstRow.add(new JLabel("First Name"));
+		firstRow.add(new JLabel("Last Name"));
+		firstRow.add(this.txtFirstName);
+		firstRow.add(this.txtLastName);
+
+		this.customerInformationPanel.add(firstRow);
+
+		JPanel secondRow = new JPanel(new GridLayout(2, 3, 5, 2));
+		secondRow.add(new JLabel("Address"));
+		secondRow.add(new JLabel("City, Province"));
+		secondRow.add(new JLabel("Postal Code"));
+
+		JPanel addressFields = new JPanel(new BorderLayout());
+		addressFields.add(this.txtAddressHouseNumber, BorderLayout.WEST);
+		addressFields.add(this.txtAddressStreet, BorderLayout.CENTER);
+		addressFields.add(this.txtAddressStreetSufix, BorderLayout.EAST);
+
+		secondRow.add(addressFields);
+
+		JPanel cityProvinceFields = new JPanel(new GridLayout(1, 2, 1, 1));
+		cityProvinceFields.add(this.txtAddressCity);
+		cityProvinceFields.add(this.cboAddressProvince);
+
+		secondRow.add(cityProvinceFields);
+
+		secondRow.add(this.txtAddressPostalCode);
+
+		this.customerInformationPanel.add(secondRow);
+
+		JPanel thirdRow = new JPanel(new GridLayout(2, 2, 5, 5));
+
+		thirdRow.add(new JLabel("Phone Number"));
+		thirdRow.add(new JLabel("Email Address:"));
+
+		JPanel phoneNumberFields = new JPanel(new FlowLayout());
+
+		phoneNumberFields.add(new JLabel("("));
+		phoneNumberFields.add(this.txtPhoneAreaCode);
+		phoneNumberFields.add(new JLabel(") "));
+		phoneNumberFields.add(this.txtPhonePrefix);
+		phoneNumberFields.add(new JLabel("-"));
+		phoneNumberFields.add(this.txtPhoneLine);
+
+		thirdRow.add(phoneNumberFields);
+
+		JPanel emailFields = new JPanel(new FlowLayout());
+
+		emailFields.add(this.txtEmailAddress);
+		emailFields.add(new JLabel("@"));
+		emailFields.add(this.txtEmailAddressDomain);
+		emailFields.add(new JLabel("."));
+		emailFields.add(this.txtEmailAddressTLD);
+
+		thirdRow.add(emailFields);
+
+		this.customerInformationPanel.add(thirdRow);
+
+		//Add the panels to the frame
 		this.add(this.customerInformationPanel);
-		
+
 	}//End of initializeGUI method
 }//End of class
